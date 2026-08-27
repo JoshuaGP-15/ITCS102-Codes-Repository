d76db2c936cd2a3796f272e0b558c6fe34de1463
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-08-27 18:47:14 +0700

    Create activity5.py

---
+++ activity5.py
@@ -0,0 +1,16 @@
+a = 40
+b = 27
+sum = a + b 
+diff = a - b
+div = a / b
+sub = a - b
+modulus = a // b 
+exponentation = a ** b
+
+print("the sum of",a,"plus",b,"is", sum)
+print("the sum of",a,"minus",b,"is", diff)
+print("the sum of",a,"times",b,"is", a*b)
+print("the sum of",a,"div",b,"is", a/b)
+print("the sum of",a,"minus",b,"is", sub)
+print("the sum of",a,"rounded",b,"is", a//b)
+print("the sum of",a,"exponent",b,"is", a**b)
