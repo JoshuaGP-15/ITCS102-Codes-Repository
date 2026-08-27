0625fe51ac054c905d0ed63d2361ed752f08ebac
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-08-27 18:29:48 +0700

    Update code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,1 +1,4 @@
+name = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\tHello my name is\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\tJoshua G. Pauyo\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
 
+
+print(name)
