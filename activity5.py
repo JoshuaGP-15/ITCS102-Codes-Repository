f72f37975311a6fc5fae9593fd7371b167c94912
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-08-27 22:42:02 +0800

    Update activity5.py

--- activity5.py
+++ activity5.py
@@ -1,16 +1,18 @@
 a = 40
 b = 27
-sum = a + b 
-diff = a - b
-div = a / b
+Add = a + b 
 sub = a - b
-modulus = a // b 
+multiplication = a * b
+div = a / b
+floor division = a // b 
 exponentation = a ** b
+modulus = a % b
 
-print("the sum of",a,"plus",b,"is", sum)
+print("the sum of",a,"plus",b,"is", add)
 print("the sum of",a,"minus",b,"is", diff)
 print("the sum of",a,"times",b,"is", a*b)
 print("the sum of",a,"div",b,"is", a/b)
 print("the sum of",a,"minus",b,"is", sub)
 print("the sum of",a,"rounded",b,"is", a//b)
 print("the sum of",a,"exponent",b,"is", a**b)
+print("the sum of",a,"remainder",b,"is",mdulus)
