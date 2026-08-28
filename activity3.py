9538b9fffe604ebce371a51ace2a75227c5c4214
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-08-28 11:23:20 +0000

    Update activity3.py

--- activity3.py
+++ activity3.py
@@ -1,1 +1,19 @@
+#string variable
 
+name = "Joshua G Pauyo"
+
+#integer variable
+age = "18"
+
+#float variable
+height = "177.66"
+weight = "54.1"
+
+#bolean variable
+isaBoy = True
+
+#print
+print("Hi my name is", name)
+print("i am", age,"years old")
+print("i am a boy", isaBoy)
+print("i am", height,"tall and i weight", weight) 
