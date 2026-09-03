808deccc9d72656f577f76f80387732ddee8d7b4
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-09-03 12:24:16 +0800

    Create activity7.py

---
+++ activity7.py
@@ -0,0 +1,13 @@
+Favorites = ''
+
+f = input("what are your favorite fruits -->")
+Favorites += f + ','
+
+f = input("what are your favorite fruits -->")
+Favorites += f + ','
+
+f = input("what are your favorite fruits -->")
+Favorites += f + ','
+
+
+print('So my favorite fruits are', Favorites)
