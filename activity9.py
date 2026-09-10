7caa1872d6ca6419de39500b9e7eff631897dea6
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-09-10 11:34:51 +0800

    Update activity9.py

--- activity9.py
+++ activity9.py
@@ -1,1 +1,25 @@
+y = 10
+x = 5
+text1 = 'Hello'
+text2 = 'Hillo'
+#1
+print(x > y)
+
+#2
+print(x < y)
+
+3#
+print(x >= y)
+
+#4
+print(x <= y)
+
+#5
+print(x == y)
+
+#Can only be used on a string
+print( text1 == text2)
+
+#6
+print( text1 != text2)
 
