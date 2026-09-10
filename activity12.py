9b34b3ca32de9e43719addfaf3d0b18c62dcfbb1
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-09-10 11:37:24 +0800

    Add user authentication with username and password

--- activity12.py
+++ activity12.py
@@ -1,1 +1,11 @@
+import getpass
+username = 'bebemo143'
+password = 'password124'
 
+u = input("Enter your Username ---")
+p = getpass.getpass("Enter your Password ---")
+
+if username == u and p == password :
+          print('Acces Allowed')
+else :
+          print('Acces Denied')
