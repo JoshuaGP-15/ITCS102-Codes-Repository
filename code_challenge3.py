c59c56acf92f2a38b075b9423f5da8fe67314bd9
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-09-10 21:05:23 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,23 +1,23 @@
 name = input('Enter Your Name --- ')
 item = input('Enter Item Name --- ')
 
-isFragile = bool(input('Handle with care --- '))
+isFragile = bool(eval(input('Handle with care (True or False) --- ')))
 distance = float(input('What is the distance? --- '))
 weight = float(input('Weight of the item --- '))
-is_express = bool(input('As soon as possible? --- '))
-is_international = bool(input('International? --- '))
+is_express = bool(eval(input('As soon as possible? (True or False)--- ')))
+is_international = bool(eval(input('International? (True or False) --- ')))
 
 base_cost = (weight * 2.50) + (distance * 0.15)
 
-free_shipping = weight <= 2.0 or distance <= 100 and not is_express and not is_international
+free_shipping = weight <= 2 and distance <= 100 and is_express == False and is_international == False
 
 if free_shipping:
     total_cost = 0.00
 
-elif is_international and is_express:
+elif is_international == True and is_express == True:
     total_cost = (base_cost * 1.40) + 50
 
-elif is_express or (is_international and weight > 20):
+elif is_express == True or  (is_international ==  True and weight > 20):
     total_cost = (base_cost * 1.20) + 25
 
 elif weight > 30 or distance > 1000:
@@ -26,6 +26,8 @@ elif weight > 30 or distance > 1000:
 else:
     total_cost = base_cost
 
+
+#print
 print('\n\t\t\t••• RECEIPT •••')
 print('Sender Name ---', name)
 print('Item ---', item)
