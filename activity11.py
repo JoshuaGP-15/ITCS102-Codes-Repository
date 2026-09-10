e0f21ffbc34dc3b0732f4fd30837b9d794ddd997
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-09-10 11:36:46 +0800

    Add username and password authentication

--- activity11.py
+++ activity11.py
@@ -1,1 +1,10 @@
+username = 'Userlangwalangdru'
+password = 'password124'
 
+u = input("Enter your Username ---")
+p = input("Enter your Password ---")
+
+if username == u and p == password :
+          print('Acces Allowed')
+else :
+          print('Acces Denied')
